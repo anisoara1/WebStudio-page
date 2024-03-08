c0e5c299115f64066856d96d8d9d59c2ebca251f
anisoara1 <122683089+anisoara1@users.noreply.github.com>
2024-03-08 09:49:00 +0200

    Update Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -38,7 +38,7 @@ export const Header = forwardRef(({ toggleDarkMode }, ref) => {
     <Box
       ref={ref}
       sx={{
-        height: '80px',
+        height: '82px',
         bgcolor: 'background.default',
         color: 'text.primary',
         borderBottom: `2px solid ${borderShadow}`,
